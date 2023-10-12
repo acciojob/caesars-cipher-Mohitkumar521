e7680d26d12500fb3ad6366d873c61a304927f97
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-12 12:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,20 @@
-// Your Script here.
-
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
-
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return; //return decodedArr
+function rot13(str) {
+  // Split the string into an array of characters
+  return str.split('').map(function(char) {
+    // Check if the character is an uppercase letter
+    if (char.match(/[A-Z]/)) {
+      // Use the ASCII code to determine the new character after ROT13
+      // Rotate 13 positions, wrapping around if necessary
+      return String.fromCharCode(((char.charCodeAt(0) - 65 + 13) % 26) + 65);
+    } else {
+      // If the character is not an uppercase letter, leave it unchanged
+      return char;
+    }
+  }).join(''); // Join the characters back into a string
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// Example usage:
+const encodedString = "URYYB JBEYQ";
+const decodedString = rot13(encodedString);
+console.log(decodedString); // Output: "HELLO WORLD"
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
